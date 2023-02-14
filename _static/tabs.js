676801d4e6ec22008d93c6b101871ff251fe9c02
Jianjie Liu <jianjl@microsoft.com>
2023-02-14 17:54:43 -0500

    Update documentation

--- _static/tabs.js
+++ _static/tabs.js
@@ -15,6 +15,7 @@ function ready() {
 function onLabelClick() {
   // Activate other labels with the same text.
   for (label of labels_by_text[this.textContent]) {
+    if (label === this) continue;
     label.previousSibling.checked = true;
   }
 }
